dc92f6c4973a7ec19e2f5a382da02c8ba72972d1
Lyqueen <58933390+fikrullaily@users.noreply.github.com>
2022-05-13 01:54:10 +0700

    update profile

--- pages/components/Navsidebar.js
+++ pages/components/Navsidebar.js
@@ -25,7 +25,7 @@ const Navsidebar = () => {
           <div className="sidenav-header">
             <i className="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
             <a className="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
-              <img src="/logo-default-428x112.png" className="navbar-brand-img h-100 mx-auto" alt="main_logo"/>
+              <img src="/logo.png" className="navbar-brand-img h-100 mx-auto" alt="main_logo"/>
             </a>
           </div>
           <hr className="horizontal dark mt-0"/>
