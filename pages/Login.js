ae26c5785f55534fee0eb234f5fffc676c6ad675
Lyqueen <58933390+fikrullaily@users.noreply.github.com>
2022-05-16 00:29:14 +0700

    terbaru

--- pages/Login.js
+++ pages/Login.js
@@ -81,7 +81,7 @@ export default function Login() {
               </div>
             </div>
             <div className="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-              <div className="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" >
+              <div className="position-relative bg-gradient-primary h-100 m-3 px-7 border-npmradius-lg d-flex flex-column justify-content-center overflow-hidden" >
                 <span className="mask bg-gradient-primary opacity-6"></span>
                 <h4 className="mt-5 text-white font-weight-bolder position-relative">"Attention is the new currency"</h4>
                 <p className="text-white position-relative">The more effortless the writing looks, the more effort the writer actually put into the process.</p>
